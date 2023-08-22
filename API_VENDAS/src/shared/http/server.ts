5f42c719346beed539bb2351afbbbd5dcba1e42c
joaovictormesquita <joaovictor.mesquita@avalgestao.com.br>
2023-08-22 15:48:29 -0300

    Add middleware to handle app exceptions

--- API_VENDAS/src/shared/http/server.ts
+++ API_VENDAS/src/shared/http/server.ts
@@ -1,1 +1,32 @@
-console.log("hello world");
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import routes from "./routes";
+import AppError from "@shared/errors/AppError";
+
+const PORT = 3333;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+// AppErrors middleware
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    const isAppError = error instanceof AppError;
+
+    const responseStatus = isAppError ? error.statusCode : 500;
+    const responseMessage = isAppError
+      ? error.message
+      : "Internal server error.";
+
+    return response.status(responseStatus).json({
+      status: "error",
+      message: responseMessage,
+    });
+  },
+);
+
+app.listen(PORT, () => console.log(`Server start at por ${PORT} `));
